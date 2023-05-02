4ea2cec99c99a6f98ae4ef2b7f61413c6e12bc0e
atnzpe <gleysonasilva@gmail.com>
2023-05-02 20:23:42 -0300

    adiciona botão mostra nome na segunda tela

--- app/src/main/java/com/example/aula04/SegundaActivity.java
+++ app/src/main/java/com/example/aula04/SegundaActivity.java
@@ -1,5 +1,7 @@
 package com.example.aula04;
 
+import static com.example.aula04.MainActivity.NOME;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,14 +10,26 @@ import android.view.View;
 
 public class SegundaActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda);
+
     }
 
-    public void inicio(View view){
+    public void inicio(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+
+    public void mostrarNome(View view) {
+        Intent intentRecebida = getIntent();
+        String nome = intentRecebida.getStringExtra(NOME);
+        intentRecebida.putExtra(NOME, nome);
+        startActivity(intentRecebida);
+
+
     }
 }
