7626ea1dd9ce5b22433bd9fd5994b3fd80fcee6b
atnzpe <gleysonasilva@gmail.com>
2023-05-09 13:42:11 -0300

    escolhe uma quantidade de numeros e sorteia eles

--- app/src/main/java/com/example/telavalidacao/MainActivity.java
+++ app/src/main/java/com/example/telavalidacao/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.telavalidacao;
 
+import static android.text.TextUtils.isEmpty;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,10 +21,15 @@ public class MainActivity extends AppCompatActivity {
         Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 EditText editText = findViewById(R.id.editText);
                 if (editText.getText().toString().isEmpty()) {
                     editText.setError("Preencha este campo!");
+                }
+                else{
+                    int x  = Integer.parseInt(editText.getText().toString());
+                  int numero = new Random().nextInt(x);
+                    editText.setText("O número sorteado é " +  numero);
                 }
             }
         });
