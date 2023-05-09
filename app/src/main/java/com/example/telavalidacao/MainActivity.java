d284fcf4ead03c3f9ebcd5941e7df7e3819dcb10
atnzpe <gleysonasilva@gmail.com>
2023-05-09 11:44:19 -0300

    cria o corpo do codigo

--- app/src/main/java/com/example/telavalidacao/MainActivity.java
+++ app/src/main/java/com/example/telavalidacao/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.telavalidacao;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +15,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editText = findViewById(R.id.editText);
+                if (editText.getText().toString().isEmpty()) {
+                    editText.setError("Preencha este campo!");
+                }
+            }
+        });
 
     }
 }
